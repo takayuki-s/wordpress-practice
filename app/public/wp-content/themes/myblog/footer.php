061aedc547fc51ff10906d553187dcbe17f0b293
takayuki-s <taka12yuki04@gmail.com>
2021-07-13 21:34:43 +0900

    header/footer部分を分けてテンプレート化

--- app/public/wp-content/themes/myblog/footer.php
+++ app/public/wp-content/themes/myblog/footer.php
@@ -3,6 +3,6 @@
 <script src="<?php echo get_template_directory_uri(); ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Custom scripts for this template -->
-<script src="js/clean-blog.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/clean-blog.min.js"></script>
 
 <?php wp_footer(); ?>
