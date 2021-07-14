ac9651178d8ba47528761d945b6837852e3b6290
takayuki-s <taka12yuki04@gmail.com>
2021-07-14 21:32:00 +0900

    アイキャッチ画像を設定できるようにする

--- app/public/wp-content/themes/myblog/single.php
+++ app/public/wp-content/themes/myblog/single.php
@@ -34,6 +34,7 @@
         <div class="container">
           <div class="row">
             <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(array(100,100), array('alt'=>'アイキャッチ画像')); ?>
               <?php the_content(); ?>
             </div>
           </div>
